2d3a373b1840f01c07e86921b3f0ea3c7523c73c
AndrewDRichter <andrewrichter1@hotmail.com>
2024-05-25 11:16:43 -0400

    a

--- first-flet-app/main.py
+++ first-flet-app/main.py
@@ -1,24 +1,49 @@
 import flet as ft
+import sqlite3
+
+class Conta():
+    def __init__(self, titular:str, valor:float=0.0):
+        self.titular = titular
+        self.valor = valor
+        self.ativa = True
 
 
 def main(page: ft.Page):
     page.title = "Aplicativo"
-    #page.scroll = "always"
-
-    def altera_contador(e):
-        if e.control.text == "-":
-            txt_contador.value = int(txt_contador.value) - 1
-        else:
-            txt_contador.value = int(txt_contador.value) + 1
-        if txt_contador.value == 100:
-            txt_contador.width = int(txt_contador.width) + 20
-        txt_contador.update()
-            
+    page.scroll = "always"
+    banco = sqlite3.connect('test_db.db')
+    cursor = banco.cursor()
+    #cursor.execute("CREATE TABLE IF NOT EXISTS tbl_teste (id INTEGER PRIMARY KEY, nome TEXT, ativo INTEGER)")
+    #cursor.execute("INSERT INTO tbl_teste (nome, ativo) VALUES (?, ?)", ("Andrew", 1))
+    #banco.commit()
+        
+    def carrega_dados_teste():
+        cursor.execute("SELECT * FROM tbl_teste")
+        slct_results = cursor.fetchall()
+        lv_teste.controls.append(ft.Row(controls=[
+            ft.Text("Id"),  
+            ft.Text("Nome"),  
+            ft.Text("Ativo"),  
+            ]))
+        lv_teste.controls.append(ft.Row(controls=[
+            ft.Divider()
+        ]))
+        for row in slct_results:
+            (id, nome, ativo) = row
+            lv_teste.controls.append(ft.Row(controls=[
+                ft.Text(id),
+                ft.Text(nome),
+                ft.Text(ativo),
+            ]))
+        lv_teste.update()
     
     def rem_espaco_duplo(e):
         valor = e.control.value.replace("  ", " ")
         nova_tarefa.value = valor
         nova_tarefa.update()
+        if valor[-1] == " ":
+            valor[-1] = ""
+        print(valor + "->" + valor[-1])
     
     def lista_controls(e):
         for i in page.controls:
@@ -51,11 +76,24 @@ def main(page: ft.Page):
 
     #page.add(ft.ElevatedButton("?", on_click=lista_controls))
     
-    lv_tarefas = ft.ListView(height=400, spacing=0, auto_scroll=True)
+    lv_tarefas = ft.ListView(height=100, spacing=0, auto_scroll=True)
     page.add(lv_tarefas)
+
+    lv_teste = ft.ListView(height=200, spacing=0, auto_scroll=True)
+    page.add(lv_teste)
+
+    carrega_dados_teste()
 
 
 ft.app(main)
+    # def altera_contador(e):
+    #     if e.control.text == "-":
+    #         txt_contador.value = int(txt_contador.value) - 1
+    #     else:
+    #         txt_contador.value = int(txt_contador.value) + 1
+    #     if txt_contador.value == 100:
+    #         txt_contador.width = int(txt_contador.width) + 20
+    #     txt_contador.update()
 
     # btn_menos = ft.ElevatedButton("-", on_click=altera_contador, bgcolor="red")
     # txt_contador = ft.TextField(value=0, width=50, input_filter=ft.NumbersOnlyInputFilter(), fill_color="black")
